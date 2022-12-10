2639b4bd75e28ec46311adf14da6b7fb90795871
JeromeGDev <jeromegdev@gmx.fr>
2022-12-10 08:40:19 +0100

    Links.json : inutile, donc supprimé - moduleMenu JS : ajout des commentaires

--- assets/js/moduleMenuJs.js
+++ assets/js/moduleMenuJs.js
@@ -1,37 +1,54 @@
-(function(){
-/* =================================== HEADER ===================================*/
-// Sélection cu conteur de nav
-const navContainer = document.querySelector( ".navContainer" );
+( function () {    // Fonction qui s'auto-appelle pour éviter collision et sécurité
+    // Définition de la valeur du statut d'execution de la fonction de débugage de l'animation : 
+    let introDebugStatus = false;
 
-console.log( "navContainer", navContainer )
+/* ================================================ Variables ================================================*/
 
-// Sélection du menu Burger
-const btnBurger = document.querySelector( ".btnBurger" );
-console.log( "btnBurger", btnBurger )
+const navContainer = document.querySelector( ".navContainer" );                 // Sélection cu conteur de nav
+const btnBurger = document.querySelector( ".btnBurger" );                       // Sélection du menu Burger
+const btnClose = document.querySelector( ".btnClose" );                         // Sélection de la croix de fermeture de la nav
 
-// Sélection de la croix de fermeture de la nav
-const btnClose = document.querySelector( ".btnClose" );
-console.log("btnClose",btnClose)
-
-// Masque la nav au démarrage
-// navContainer.classList.add( "isHidden" );
+    
+/* =================================== écouteurs / déclencheur d'évènements ===================================*/
 
 // Toggle de la nav :
-// à partir du bouton burger
-btnBurger.addEventListener( "click", toggleNav );
+    // à partir du bouton burger
+    btnBurger.addEventListener( "click", toggleNav );                           // ajout d'un écouteur d'évènement à partir du bouton burger
+    // à partir du bouton de fermeture (croix)
+    btnClose.addEventListener( "click", toggleNav );                            // à partir du bouton croix de fermeture
 
-// à partir du bouton croix
-btnClose.addEventListener( "click", toggleNav );
 
-// fonction de toggle de la nav
-function toggleNav(){
-    if (navContainer.classList.contains( "hideMenu" )){
-        navContainer.classList.remove( "hideMenu" );
-        navContainer.classList.toggle( "isHidden" );
-    } else {
-        navContainer.classList.toggle( "isHidden" );
+/* ================================================= Fonctions =================================================*/
+    // fonction de toggle de la nav
+    function toggleNav(){
+        if (navContainer.classList.contains( "hideMenu" )){                     // Si le conteneur de la nav contient la classe "hideMenu"
+            navContainer.classList.remove( "hideMenu" );                        // Supprime la classe "hideMenu"
+            navContainer.classList.toggle( "isHidden" );                        // Toggle la classe la classe "isHidden" qui définit le statut d'affichage de la nav
+        } else {                                                                // Sinon
+            navContainer.classList.toggle( "isHidden" );                        // Toggle directement la classe la classe "isHidden" qui définit le statut d'affichage de la nav
+        }
     }
-}
+
+    // On appelle la fonction hashchange dés le démarrage de la page
+    // fonction d'execution de la fonction de débugage : en fonction de la valeur du statut, affiche les valeurs de débugage dans la console. Ne s'excute que si la valeur est modifiée manuellement sur "true"
+    function introDebugLanch() {
+        if ( introDebugStatus !== false ) {                                     // Si le statut de lancement de la fonction de débugage n'est pas faux,
+            introDebug();                                                       //  lance le débugage
+        }
+    }
+
+
+    // fonction de débugage dans la console : en fonction de la valeur du statut,
+    function introDebug() {
+
+        console.log( "navContainer", navContainer );
+        console.log( "btnBurger", btnBurger );
+        console.log("btnClose",btnClose);
+    }
+
+    /* ========== appel des fonctions ========== */
+    // appel de la fonction d'execution de la fonction de débugage
+    introDebugLanch()
 
 
 })()
